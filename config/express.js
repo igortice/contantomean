b771ab9df16c7d950b2030330009bd4f5dd504f1
Igor Rocha <igortice@gmail.com>
2015-04-08 11:56:45 -0300

    base config express ok

--- config/express.js
+++ config/express.js
@@ -3,5 +3,11 @@ var express    = require('express');
 
 module.exports = function () {
   var app = express();
+  // config ambiente
+  app.set('port', 3000);
+
+  // middleware
+  app.use(express.static('./public'));
+
   return app;
 };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+// config index
 var http = require('http');
 var app  = require('./config/express')();
 
 http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server port' + app.get('port'));
+  console.log('Express server port : ' + app.get('port'));
 });
